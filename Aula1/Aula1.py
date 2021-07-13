29610365badc31e2e9111f3aedf41447e0e6b2ec
jorge-trindade <jorginhogdr@gmail.com>
2021-07-13 17:11:42 -0300

    init

---
+++ Aula1/Aula1.py
@@ -0,0 +1,26 @@
+'''
+
+Name: Jorge
+Project: mypython
+State: São Paulo
+
+'''
+#Thafirst
+x=2 #declare
+j=("Thanks")
+
+print(j)
+print(x)
+print(x+x)
+
+print("Full")
+print("All?")
+print("Devsecops?")
+
+
+#datamodificate
+x = 3
+
+
+
+
---
+++ Aula1/modificatedata.py
@@ -0,0 +1,7 @@
+x = str (3) #textstring
+y = int (4) #calcmat
+z = float (5) #transformtofloat
+
+print (x + x)
+print (y + y)
+print (z + z)

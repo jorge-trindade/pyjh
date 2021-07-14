52ae515a462f06799ffc0e358d720a20555ae1d3
jorge-trindade <jorginhogdr@gmail.com>
2021-07-14 00:40:51 -0300

    adjust

---
+++ projectActions/interactInputUser.py
@@ -0,0 +1,8 @@
+name = input ('Whats your name? ') #in this case it will ask the user to write the name to return the sentence
+age = input ('How old are you? ')
+age = str(age) #it was necessary to convert the number to string for the code to compile
+city = input ('Where you from my brotha? ')
+
+print('The ' + name + ' have ' + age + ' years old and lives in ' + city + '.')
+# this is a basic way to print a sentence with string and integers
+

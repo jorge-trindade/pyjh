080e1e61c0eb901f4ccafab463e4bf0f49d04b6d
jorge-trindade <jorginhogdr@gmail.com>
2021-07-14 00:30:46 -0300

    strings and integers

---
+++ projectActions/stint.py
@@ -0,0 +1,8 @@
+name = 'Malcom X'
+age = 32
+age = str(age) #it was necessary to convert the number to string for the code to compile
+city = 'São Paulo'
+
+print('The ' + name + '  ' + age + ' years old and lives in ' + city + '.')
+# this is a basic way to print a sentence with string and integers
+
